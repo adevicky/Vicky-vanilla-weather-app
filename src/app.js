6608f735b23f220ffd2371709c7b45e89211423b
Adenuga Victoria <adenugavictoria29@gmail.com>
2022-05-19 22:27:25 +0000

    Added apiKey, apiUrl and function to search form

--- src/app.js
+++ src/app.js
@@ -14,4 +14,29 @@ return `${day}, ${hours}:${minutes}`;
 let currentWeather= new Date();
 let realTme= document.querySelector("#current-date");
 realTme.innerHTML=formattedTime(currentWeather);
-fdh
+
+function displayWeatherCondition(response) {
+  document.querySelector("#location").innerHTML =response.data.name;
+    document.querySelector("#temprature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#description").innerHTML = response.data.weather[0].main;
+     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+      document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    }
+
+function searchCurrentCity(event) {
+    event.preventDefault();
+    let city= "New York";
+    let newCity= document.querySelector("#city-input").value
+search(city)
+}
+
+
+let searchForm= document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCurrentCity);
+
+function searchLocation(position) {
+   let apiKey= "6e28aff5cc0608c0525e4afec6ebc572";
+   let apiUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+   axios.get(apiUrl).then(displayWeatherCondition);
+}
+search(city);
